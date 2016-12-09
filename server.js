b4e0a6a345bb4ff624ae4861ae5d475de6df9233
loven <loven@oregonstate.edu>
2016-12-09 13:38:34 -0800

    fixing layout

--- server.js
+++ server.js
@@ -12,13 +12,32 @@ app.set("view engine", "handlebars");
 
 // serve static files from public/
 app.use(express.static(path.join(__dirname, "public")));
+// set views
+app.set("views", path.join(__dirname, "views"));
+  
+
+// state variable to store character set currently being used
+var currentSet = "1";
+
+// state variable to store position of current character being viewed
+var currentChar = 0;
+
+// state variable to store position of current pronunciation being viewed
+var currentPron = 1;
+
+// state variable to store position of current meaning being viewed
+var currentMean = 2;
 
 // route root path to index page, and give handlebars the appropriate page
 // title and chinese data to render
 app.get("/", function(req, res) {
   res.render("main", {
     title: "Chinese Learning App",
-    characters: characters
+    characters: characters,
+    currentSet: currentSet,
+    currentChar: currentChar,
+    currentPron: currentPron,
+    currentMean: currentMean
   });
 });
 
